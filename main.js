461b1445bf3ba43d1d576995c3c1d19590e05047
Voxarth <pain.n.blood@gmail.com>
2019-03-12 14:35:15 +0000

    V2, ajout d'un burger menu, correction de la problématique lié au .style.visibility + début du scroll by event

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 function menuBurger(){
 
- var monmenu = document.getElementById("nav")
+ var menu = document.getElementById("nav");
 
- monmenu.style.visibility = ( monmenu.style.visibility =="hidden")
-                             ? "initial" : "hidden";
+ if (menu.classList.contains("hidden")){
+    menu.classList.add("visible");
+    menu.classList.remove("hidden");
+ }else if(menu.classList.contains("visible"))  {
+     menu.classList.remove("visible");
+     menu.classList.add("hidden");
 
+ }
 
 }
+
+
+document.addEventListener("scroll", pinedNavbar);
+
+function pinedNavbar{
+
+    var navbar = document.getElementById("nav");
+
+    if (navbar.classList.contains("fixed")){
+        navbar.classList.add()
+    }
+}
+
+

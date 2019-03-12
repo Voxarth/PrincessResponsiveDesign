98ebb61309bb1dc47d1c9ee9772737e840496408
Voxarth <pain.n.blood@gmail.com>
2019-03-12 10:58:28 +0000

    V2, ajout d'un burger menu

---
+++ main.js
@@ -0,0 +1,9 @@
+function menuBurger(){
+
+ var monmenu = document.getElementById("nav")
+
+ monmenu.style.visibility = ( monmenu.style.visibility =="hidden")
+                             ? "initial" : "hidden";
+
+
+}
